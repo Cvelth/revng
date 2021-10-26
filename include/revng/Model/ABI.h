43e7b8ff8ef891ef231dd3348b842e76ad00af2d
Ivan Krysak <ivan@rev.ng>
2021-10-26 16:08:57 +0200

    Add ABI explanation comments

--- include/revng/Model/ABI.h
+++ include/revng/Model/ABI.h
@@ -14,26 +14,92 @@
 namespace model::abi {
 
 enum Values {
+  /// Unknown and/or unsupported ABI.
   Invalid,
 
+  /// 64-bit SystemV ABI for x86 processor architecure.
+  /// The latest version of the documentation can be found
+  /// [here](https://gitlab.com/x86-psABIs/x86-64-ABI)
   SystemV_x86_64,
-  SystemV_x86,
-  SystemV_x86_regparm_3,
-  SystemV_x86_regparm_2,
-  SystemV_x86_regparm_1,
-  // SystemV_x86_sseregparm_3,
-  // SystemV_x86_sseregparm_2,
-  // SystemV_x86_sseregparm_1,
 
+  /// 32-bit SystemV ABI for x86 processor architecture.
+  /// The latest version of the documentation can be found
+  /// [here](https://gitlab.com/x86-psABIs/i386-ABI/-/tree/hjl/x86/master)
+  SystemV_x86,
+
+  /// A gcc specific modification of the 32-bit SystemV ABI for x86 processor
+  /// architecture. It allows three first GPR-sized arguments to be passed
+  /// using the EAX, EDX, and ECX registers.
+  /// \see `regparm` x86 function attribute.
+  SystemV_x86_regparm_3,
+
+  /// A gcc specific modification of the 32-bit SystemV ABI for x86 processor
+  /// architecture. It allows two first GPR-sized arguments to be passed
+  /// using the EAX, and ECX registers.
+  /// \see `regparm` x86 function attribute.
+  SystemV_x86_regparm_2,
+
+  /// A gcc specific modification of the 32-bit SystemV ABI for x86 processor
+  /// architecture. It allows the first GPR-sized argument to be passed
+  /// using the EAX register.
+  /// \see `regparm` x86 function attribute.
+  SystemV_x86_regparm_1,
+
+  /// 64-bit Microsoft ABI for x86 processor architecture.
+  /// The documentation can be found
+  /// [here](https://docs.microsoft.com/en-us/cpp/build/x64-calling-convention)
   Microsoft_x64,
+
+  /// A modification of 64-bit Microsoft ABI for x86 processor architecture.
+  /// It allows using extra vector registers for passing function arguments.
+  /// The documentation can be found
+  /// [here](https://docs.microsoft.com/en-us/cpp/cpp/vectorcall)
   Microsoft_x64_vectorcall,
+
+  /// A modification of 64-bit Microsoft ABI for x86 processor architecture.
+  /// It uses CLR expression stack to pass function arguments.
+  /// The documentation can be found
+  /// [here](https://docs.microsoft.com/en-us/cpp/cpp/clrcall)
   Microsoft_x64_clrcall,
 
+  /// The default 32-bit Microsoft ABI for x86 processor architecture.
+  /// It was indented to be compatible with `SystemV_x86` but there are slight
+  /// differences. The documentation can be found
+  /// [here](https://docs.microsoft.com/en-us/cpp/cpp/cdecl)
   Microsoft_x86_cdecl,
+
+  /// A modification of the 32-bit `__cdecl` Microsoft ABI for x86 processor
+  /// architecture. The main difference is the fact that the callee is
+  /// responsible for stack cleanup instead of the caller.
+  /// The documentation can be found
+  /// [here](https://docs.microsoft.com/en-us/cpp/cpp/stdcall)
   Microsoft_x86_stdcall,
+
+  /// A modification of the 32-bit `__stdcall` Microsoft ABI for x86 processor
+  /// architecture. The main difference is the fact that it allows to pass a
+  /// single (the first) function argument using a register. This ABI is only
+  /// used for member function call where the first argument is always a `this`
+  /// pointer. The documentation can be found
+  /// [here](https://docs.microsoft.com/en-us/cpp/cpp/thiscall)
   Microsoft_x86_thiscall,
+
+  /// A modification of the 32-bit `__stdcall` Microsoft ABI for x86 processor
+  /// architecture. The main difference is the fact that it allows to pass two
+  /// first GPR-sized non-aggregate function arguments in registers.=
+  /// The documentation can be found
+  /// [here](https://docs.microsoft.com/en-us/cpp/cpp/fastcall)
   Microsoft_x86_fastcall,
+
+  /// A modification of 32-bit Microsoft ABI for x86 processor architecture.
+  /// It uses CLR expression stack to pass function arguments.
+  /// The documentation can be found
+  /// [here](https://docs.microsoft.com/en-us/cpp/cpp/clrcall)
   Microsoft_x86_clrcall,
+
+  /// A modification of the 32-bit `__fastcall` Microsoft ABI for x86 processor
+  /// architecture. It allows using extra vector registers for passing function
+  /// arguments. The documentation can be found
+  /// [here](https://docs.microsoft.com/en-us/cpp/cpp/vectorcall)
   Microsoft_x86_vectorcall,
 
   Count
