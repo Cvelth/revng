c182ea0c16f10be6351cd6c26b9f5af445d45e23
Ivan Krysak <ivan@rev.ng>
2022-05-11 15:44:31 +0300

    Outline sugiyama style layouter interface

---
+++ lib/Yield/Support/SugiyamaStyleGraphLayout.cpp
@@ -0,0 +1,11 @@
+/// \file SugiyamaStyleGraphLayout.cpp
+/// \brief
+
+//
+// This file is distributed under the MIT License. See LICENSE.md for details.
+//
+
+#include "revng/Yield/Support/SugiyamaStyleGraphLayout.h"
+
+bool yield::sugiyama::layout(Graph &Graph, const Configuration &Configuration) {
+}
