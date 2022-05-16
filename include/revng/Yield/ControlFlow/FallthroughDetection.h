3ebb7a01a0ca019f9c2d3e21fb6ae828e1a41191
Ivan Krysak <ivan@rev.ng>
2022-05-16 13:45:23 +0300

    Implement labeled block detection

--- include/revng/Yield/ControlFlow/FallthroughDetection.h
+++ include/revng/Yield/ControlFlow/FallthroughDetection.h
@@ -4,6 +4,8 @@
 // This file is distributed under the MIT License. See LICENSE.md for details.
 //
 
+#include "llvm/ADT/SmallVector.h"
+
 namespace model {
 class Binary;
 }
@@ -18,5 +20,11 @@ template<bool ShouldMergeFallthroughTargets>
 const yield::BasicBlock *detectFallthrough(const yield::BasicBlock &BasicBlock,
                                            const yield::Function &Function,
                                            const model::Binary &Binary);
+
+template<bool ShouldMergeFallthroughTargets>
+llvm::SmallVector<MetaAddress, 4>
+labeledBlock(const yield::BasicBlock &BasicBlock,
+             const yield::Function &Function,
+             const model::Binary &Binary);
 
 } // namespace yield::cfg
--- lib/Yield/ControlFlow/FallthroughDetection.cpp
+++ lib/Yield/ControlFlow/FallthroughDetection.cpp
@@ -34,11 +34,33 @@ yield::cfg::detectFallthrough(const yield::BasicBlock &BasicBlock,
   return Result;
 }
 
-template const yield::BasicBlock *
-yield::cfg::detectFallthrough<true>(const yield::BasicBlock &BasicBlock,
-                                    const yield::Function &Function,
-                                    const model::Binary &Binary);
-template const yield::BasicBlock *
-yield::cfg::detectFallthrough<false>(const yield::BasicBlock &BasicBlock,
-                                     const yield::Function &Function,
-                                     const model::Binary &Binary);
+template<bool ShouldMergeFallthroughTargets>
+llvm::SmallVector<MetaAddress, 4>
+yield::cfg::labeledBlock(const yield::BasicBlock &BasicBlock,
+                         const yield::Function &Function,
+                         const model::Binary &Binary) {
+  // Blocks that are a part of another labeled block cannot start a new one.
+  using namespace yield::BasicBlockType;
+  if (shouldSkip<ShouldMergeFallthroughTargets>(BasicBlock.Type))
+    return {};
+
+  llvm::SmallVector<MetaAddress, 4> Result;
+
+  constexpr bool MergeFallthrough = ShouldMergeFallthroughTargets;
+  auto Next = detectFallthrough<MergeFallthrough>(BasicBlock, Function, Binary);
+  while (Next != nullptr) {
+    Result.emplace_back(Next->Address);
+    Next = detectFallthrough<MergeFallthrough>(*Next, Function, Binary);
+  }
+
+  return Result;
+}
+
+template llvm::SmallVector<MetaAddress, 4>
+yield::cfg::labeledBlock<true>(const yield::BasicBlock &BasicBlock,
+                               const yield::Function &Function,
+                               const model::Binary &Binary);
+template llvm::SmallVector<MetaAddress, 4>
+yield::cfg::labeledBlock<false>(const yield::BasicBlock &BasicBlock,
+                                const yield::Function &Function,
+                                const model::Binary &Binary);
