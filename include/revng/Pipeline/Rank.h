d9707e323b00c887ee732ba3de4f6f5330135cf7
Ivan Krysak <ivan@rev.ng>
2022-07-26 23:14:52 +0300

    Introduce basic location support

--- include/revng/Pipeline/Rank.h
+++ include/revng/Pipeline/Rank.h
@@ -5,6 +5,7 @@
 //
 
 #include "revng/ADT/StringTemplateParameter.h"
+#include "revng/Pipeline/Location.h"
 #include "revng/Support/DynamicHierarchy.h"
 
 namespace pipeline {
@@ -17,17 +18,68 @@ public:
   Rank(llvm::StringRef Name, Rank &Parent) : DynamicHierarchy(Name, Parent) {}
 };
 
-  template<StringTemplateParameter Str, typename LocationType, typename ParentRnk>
-  class RankDefinition : public Rank {
+namespace detail {
+
+template<typename... TypesToAppend, typename... InitialTupleTypes>
+inline constexpr auto appendImpl(std::tuple<InitialTupleTypes...> &&Tuple) {
+  return std::tuple<InitialTupleTypes..., TypesToAppend...>();
+}
+
+template<typename Tuple, typename... TypesToAppend>
+struct AppendToTupleHelper {
+  using type = decltype(appendImpl<TypesToAppend...>(std::declval<Tuple>()));
+};
+
+template<typename First, typename Second>
+struct CH {
+  template<std::size_t Index>
+  constexpr bool operator()() {
+    return std::is_convertible_v<std::tuple_element_t<Index, First>,
+                                 std::tuple_element_t<Index, Second>>;
+  }
+};
+template<typename Tuple>
+inline constexpr auto TS = std::tuple_size_v<Tuple>;
+
+// clang-format off
+template<typename LHS, typename RHS>
+concept TupleConvertibilityCheck = TS<LHS> == TS<RHS> &&
+                                   constexprAnd<TS<LHS>>(CH<LHS, RHS>{});
+// clang-format on
+
+} // namespace detail
+
+template<StringTemplateParameter Str, typename LocationType, typename ParentRnk>
+class RankDefinition : public Rank {
 public:
   static constexpr std::string_view RankName = Str;
   using Type = LocationType;
   using Parent = ParentRnk;
 
   using Definition = RankDefinition<Str, Type, Parent>;
+  using RankTag = Definition;
+
+public:
+  static_assert(std::is_same_v<Parent, typename Parent::Definition>);
+  static constexpr size_t Depth = Parent::Depth + 1;
+  using Tuple = typename detail::AppendToTupleHelper<typename Parent::Tuple,
+                                                     Type>::type;
 
 public:
   explicit RankDefinition(Parent &ParentObj) : Rank(RankName, ParentObj) {}
+
+  // clang-format off
+  template<typename ...Args>
+    requires detail::TupleConvertibilityCheck<std::tuple<Args...>, Tuple>
+  constexpr static Location<Definition> makeLocation(Args &&...Arguments) {
+    // clang-format on
+    return Location<Definition>(Arguments...);
+  }
+
+  constexpr static std::optional<Location<Definition>>
+  locationFromString(std::string_view String) {
+    return Location<Definition>::fromString(String);
+  }
 };
 
 // Root rank specialization
@@ -35,12 +87,27 @@ template<StringTemplateParameter Str>
 class RankDefinition<Str, void, void> : public Rank {
 public:
   static constexpr std::string_view RankName = Str;
-  using Definition = RankDefinition<Str, void, void>;
   using Type = void;
   using Parent = void;
 
+  using Definition = RankDefinition<Str, Type, Parent>;
+  using RankTag = Definition;
+
+public:
+  static constexpr size_t Depth = 0;
+  using Tuple = std::tuple<>;
+
 public:
   explicit RankDefinition() : Rank(RankName) {}
+
+  constexpr static Location<Definition> makeLocation() {
+    return Location<Definition>{};
+  }
+
+  constexpr static std::optional<Location<Definition>>
+  locationFromString(std::string_view String) {
+    return Location<Definition>::fromString(String);
+  }
 };
 template<StringTemplateParameter Str>
 using RootRankDefinition = RankDefinition<Str, void, void>;
