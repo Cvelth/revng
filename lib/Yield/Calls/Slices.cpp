7d28ba7c65def326aa4e35499b5727c008e3f8d3
Ivan Krysak <ivan@rev.ng>
2022-08-22 09:21:46 +0300

    Rework graph slicing from the ground up

--- lib/Yield/Calls/Slices.cpp
+++ lib/Yield/Calls/Slices.cpp
@@ -14,74 +14,73 @@
 
 using NodeView = const yield::Graph::Node *;
 
-struct NodeLookupHelper {
-  const MetaAddress &TargetAddress;
-  bool operator()(NodeView Node) { return Node->Address == TargetAddress; }
-};
-
 static yield::Graph::Node *
 nodeHelper(yield::Graph &Graph, const yield::Graph::Node *Source) {
   auto New = std::make_unique<yield::Graph::Node>(Source->data());
   return Graph.addNode(std::move(New));
 }
 
-template<bool IsForwards>
+/// \tparam NV local `NodeView` specialization
+/// \tparam INV inverted location `NodeView` specialization
+template<typename NV, typename INV>
 yield::Graph
 preservingSliceImpl(const yield::Graph &Input, const MetaAddress &SlicePoint) {
-  auto Entry = llvm::find_if(Input.nodes(), NodeLookupHelper{ SlicePoint });
+  auto LookupHelper = [&SlicePoint](NodeView Node) {
+    return Node->Address == SlicePoint;
+  };
+  auto Entry = llvm::find_if(Input.nodes(), LookupHelper);
   revng_assert(Entry != Input.nodes().end());
-
-  using NV = std::conditional_t<IsForwards, NodeView, llvm::Inverse<NodeView>>;
-  using INV = std::conditional_t<IsForwards, llvm::Inverse<NodeView>, NodeView>;
-
-  // using Trait = llvm::GraphTraits<NV>;
-  using InverseTrait = llvm::GraphTraits<INV>;
 
   // Find the rank of each node, such that for any node its rank is equal to
   // the highest rank among its children plus one.
   std::unordered_map<const yield::Graph::Node *, size_t> Ranks;
-  llvm::ReversePostOrderTraversal<NodeView, InverseTrait> RPOT(*Entry);
-  for (auto Iterator = RPOT.begin(); Iterator != RPOT.end(); ++Iterator) {
-    uint64_t &CurrentRank = Ranks[*Iterator];
+  std::vector<NodeView> PostOrder(llvm::po_begin(NV{ *Entry }),
+                                  llvm::po_end(NV{ *Entry }));
+  for (NodeView CurrentNode : llvm::reverse(PostOrder)) {
+    uint64_t &CurrentRank = Ranks[CurrentNode];
 
     CurrentRank = 0;
-    for (auto ChildIterator = InverseTrait::child_begin(*Iterator);
-         ChildIterator != InverseTrait::child_end(*Iterator);
-         ++ChildIterator) {
-      auto RankIterator = Ranks.find(*ChildIterator);
-      if (RankIterator != Ranks.end())
-        if (RankIterator->second + 1 > CurrentRank)
-          CurrentRank = RankIterator->second + 1;
-    }
+    for (auto Child : llvm::children<INV>(CurrentNode))
+      if (auto RankIterator = Ranks.find(Child); RankIterator != Ranks.end())
+        CurrentRank = std::max(RankIterator->second + 1, CurrentRank);
   }
 
-  // For each node, select a single neighbour.
-  // TODO: We should choose a better selection algorithm.
+  // For each node, select a single predecessor to keep connected to.
+  // The ranks calculated earlier are used to choose a specific one.
+  //
+  // TODO: We should consider a better selection algorithm.
   std::unordered_map<NodeView, NodeView> RealEdges;
-  for (const yield::Graph::Node *Node : Input.nodes()) {
-    auto Helper = [&Ranks, Node](NodeView Neighbour) {
-      auto NodeIt = Ranks.find(Node);
-      auto NeighbourIt = Ranks.find(Neighbour);
-      if (NodeIt == Ranks.end() || NeighbourIt == Ranks.end())
-        return false;
-      else
-        return NodeIt->second > NeighbourIt->second;
-    };
+  for (NodeView Node : Input.nodes()) {
+    NodeView SelectedNeighbour = nullptr;
 
-    auto It = std::find_if(InverseTrait::child_begin(Node),
-                           InverseTrait::child_end(Node),
-                           Helper);
-    if (It != InverseTrait::child_end(Node)) {
-      auto [_, Success] = RealEdges.try_emplace(Node, *It);
-      revng_assert(Success);
-    } else {
-      auto [_, Success] = RealEdges.try_emplace(Node, nullptr);
-      revng_assert(Success);
+    // This selects a child with the highest rank
+    auto NodeIt = Ranks.find(Node);
+    if (NodeIt != Ranks.end()) {
+      size_t SelectedNeighbourRank = 0;
+      for (NodeView Neighbour : llvm::children<INV>(Node)) {
+        auto NeighbourIt = Ranks.find(Neighbour);
+        if (NeighbourIt != Ranks.end()) {
+
+          // Select the neighbour with the highest possible rank that is still
+          // lower than the current node's rank.
+          size_t Rank = NeighbourIt->second;
+          if (Rank < NodeIt->second && Rank >= SelectedNeighbourRank) {
+            SelectedNeighbour = Neighbour;
+            SelectedNeighbourRank = Rank;
+          }
+        }
+      }
     }
+
+    auto [_, Success] = RealEdges.try_emplace(Node, SelectedNeighbour);
+    revng_assert(Success);
   }
 
   yield::Graph Result;
   std::unordered_map<NodeView, yield::Graph::Node *> Lookup;
+
+  // Returns the version of the node from the new graph if it exists,
+  // or adds a new one to if it does not.
   auto FindOrAddHelper = [&Result, &Lookup](NodeView OldNode) {
     if (auto NewNode = Lookup.find(OldNode); NewNode != Lookup.end())
       return NewNode->second;
@@ -89,21 +88,24 @@ preservingSliceImpl(const yield::Graph &Input, const MetaAddress &SlicePoint) {
       return Lookup.emplace(OldNode, nodeHelper(Result, OldNode)).first->second;
   };
 
-  for (auto NodeIterator = llvm::bf_begin(NV{ *Entry });
-       NodeIterator != llvm::bf_end(NV{ *Entry });
-       ++NodeIterator) {
-    for (auto NeighbourIterator = InverseTrait::child_begin(*NodeIterator);
-         NeighbourIterator != InverseTrait::child_end(*NodeIterator);
-         ++NeighbourIterator) {
-      auto *NewNeighbour = FindOrAddHelper(*NeighbourIterator);
-      if (RealEdges.at(*NodeIterator) == *NeighbourIterator) {
-        // Emit a real edge.
-        NewNeighbour->addSuccessor(FindOrAddHelper(*NodeIterator));
-      } else {
-        // Emit a fake node.
-        auto NewNode = nodeHelper(Result, *NodeIterator);
-        NewNode->NextAddress = NewNode->Address;
-        NewNeighbour->addSuccessor(NewNode);
+  // Make sure entry node is always present so that this never returns an empty
+  // graph
+  FindOrAddHelper(*Entry);
+
+  // Fill in the `Result` graph.
+  for (NodeView Node : llvm::breadth_first(NV{ *Entry })) {
+    for (auto Neighbour : llvm::children<INV>(Node)) {
+      if (Ranks.contains(Neighbour)) {
+        auto *NewNeighbour = FindOrAddHelper(Neighbour);
+        if (RealEdges.at(Node) == Neighbour) {
+          // Emit a real edge, if this is the neighbour selected earlier.
+          NewNeighbour->addSuccessor(FindOrAddHelper(Node));
+        } else {
+          // Emit a fake node otherwise.
+          auto NewNode = nodeHelper(Result, Node);
+          NewNode->NextAddress = NewNode->Address;
+          NewNeighbour->addSuccessor(NewNode);
+        }
       }
     }
   }
@@ -118,10 +120,14 @@ preservingSliceImpl(const yield::Graph &Input, const MetaAddress &SlicePoint) {
 yield::Graph
 yield::calls::calleePreservingSlice(const yield::Graph &Input,
                                     const MetaAddress &SlicePoint) {
-  return preservingSliceImpl<true>(Input, SlicePoint);
+  // Forwards direction, makes sure no successor relation ever gets lost.
+  return preservingSliceImpl<NodeView, llvm::Inverse<NodeView>>(Input,
+                                                                SlicePoint);
 }
 yield::Graph
 yield::calls::callerPreservingSlice(const yield::Graph &Input,
                                     const MetaAddress &SlicePoint) {
-  return preservingSliceImpl<false>(Input, SlicePoint);
+  // Backwards direction, makes sure no predecessor relation ever gets lost.
+  return preservingSliceImpl<llvm::Inverse<NodeView>, NodeView>(Input,
+                                                                SlicePoint);
 }
