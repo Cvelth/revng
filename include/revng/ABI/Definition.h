7379069fe9412f1ad7f685ebcd67398a4162f65e
Ivan Krysak <ivan@rev.ng>
2022-11-28 10:49:41 +0200

    ABI: move state deduction onto the runtime trait

--- include/revng/ABI/Definition.h
+++ include/revng/ABI/Definition.h
@@ -6,6 +6,7 @@
 
 #include <vector>
 
+#include "revng/ABI/RegisterState.h"
 #include "revng/ABI/ScalarType.h"
 #include "revng/ADT/SortedVector.h"
 #include "revng/Model/ABI.h"
@@ -331,6 +332,13 @@ public:
 
   std::optional<uint64_t>
   alignment(model::VerifyHelper &VH, const model::QualifiedType &Type) const;
+
+public:
+  std::optional<abi::RegisterState::Map>
+  tryDeducingRegisterState(const abi::RegisterState::Map &State) const;
+
+  abi::RegisterState::Map
+  enforceRegisterState(const abi::RegisterState::Map &State) const;
 };
 
 } // namespace abi
